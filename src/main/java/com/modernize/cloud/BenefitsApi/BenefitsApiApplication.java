563db2501c26477430598607589407f1e5deb81d
Cheaves, Terrell (tc719j) <tc719j@us.att.com>
2021-05-05 10:38:27 -0400

    adding initial application files. closes #1

---
+++ src/main/java/com/modernize/cloud/BenefitsApi/BenefitsApiApplication.java
@@ -0,0 +1,13 @@
+package com.modernize.cloud.BenefitsApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenefitsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenefitsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/modernize/cloud/BenefitsApi/BenefitApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modernize.cloud.BenefitsApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BenefitApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
