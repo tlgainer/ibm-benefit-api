36b57193f9801d6f8bbcf74abec04e94256c660a
tlcheaves <tlcheaves@outlook.com>
2021-05-11 14:20:00 -0400

    cloudant rest controller. closes #12

---
+++ src/main/java/com/modernize/cloud/BenefitsApi/controllers/BenefitControllerCD.java
@@ -0,0 +1,51 @@
+package com.modernize.cloud.BenefitsApi.controllers;
+
+import com.modernize.cloud.BenefitsApi.model.BenefitCD;
+import com.modernize.cloud.BenefitsApi.model.BenefitRequest;
+import com.modernize.cloud.BenefitsApi.model.BenefitResponse;
+import com.modernize.cloud.BenefitsApi.services.BenefitService;
+import com.modernize.cloud.BenefitsApi.services.BenefitServiceCD;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v2/benefit")
+public class BenefitControllerCD {
+
+    @Autowired
+    private BenefitServiceCD benefitService;
+
+    @GetMapping
+    public ResponseEntity<List<BenefitCD>> getBenefit() throws Exception {
+        return new ResponseEntity<List<BenefitCD>>(benefitService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping({"/{id}"})
+    public ResponseEntity<BenefitCD> getById(@PathVariable String id) throws Exception {
+        return new ResponseEntity<BenefitCD>(benefitService.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity save(@Valid @RequestBody BenefitCD benefit) throws Exception {
+        benefitService.save(benefit);
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping({"/{id}"})
+    public ResponseEntity update(@PathVariable String id, @Valid @RequestBody BenefitCD benefit) throws Exception {
+        benefitService.update(id, benefit);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @DeleteMapping({"/{id}"})
+    public ResponseEntity delete(@PathVariable String id) throws Exception {
+        benefitService.deleteById(id);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+}
